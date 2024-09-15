b41f9682705142c9ffad065c619931e61c733f94
Sebastian <zzevazzz_1@hotmail.com>
2024-09-15 12:54:57 -0300

    third-A-Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-// import { EcCard } from "./components/EcCard/EcCard"
 import { Outlet } from "react-router-dom"
-// import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer"
-// import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer"
 import { NavBar } from "./components/NavBar/NavBar"
-// import { Gallery } from "./components/Gallery/Gallery"
 
 
 
@@ -14,7 +10,6 @@ export const App = () => {
   return (
     <>
     <NavBar/>
-    {/* <Gallery/> */}
     <div style={{ height: 70 }} /> 
    
      <Outlet />
